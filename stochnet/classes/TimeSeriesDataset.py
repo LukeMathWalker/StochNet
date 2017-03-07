6fa37f78c6f0f6cc5f27a391fd59b7b38389aeec
LukeMathWalker <LukeMathWalker@users.noreply.github.com>
2017-03-07 16:40:45 +0100

    More Tests

--- stochnet/classes/TimeSeriesDataset.py
+++ stochnet/classes/TimeSeriesDataset.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from stochnet.classes.Errors import ShapeError
 import numpy as np
 
+
 class TimeSeriesDataset:
 
     def __init__(self, dataset_address, with_timestamps=True):
@@ -20,7 +21,9 @@ class TimeSeriesDataset:
             self.nb_timesteps = self.data.shape[1]
             self.nb_features = self.data.shape[2]
         except:
-            raise ShapeError('The dataset is not properly formatted.\n We expect the following shape: [nb_trajectories, nb_timesteps, nb_features]')
+            raise ShapeError('''The dataset is not properly formatted.\n
+                              We expect the following shape:
+                              [nb_trajectories, nb_timesteps, nb_features]''')
 
     def format_dataset_for_ML(self, keep_timestamps=False, nb_past_timesteps=1,
                               must_be_rescaled=True):
@@ -35,22 +38,25 @@ class TimeSeriesDataset:
     def remove_timestamps(self):
         if self.with_timestamps is True:
             self.data = self.data[..., 1:]
-            self.nb_features = self.nb_features-1
+            self.nb_features = self.nb_features - 1
             self.with_timestamps = False
 
     def rescale(self):
-        self.scaler = StandardScaler()
-        # StandardScaler expects data of the form [n_samples, n_features]
-        flat_data = self.data.reshape(-1, self.nb_features)
-        self.data = self.scaler.fit_transform(flat_data)
-        self.rescaled = True
+        if self.rescaled is False:
+            self.scaler = StandardScaler()
+            # StandardScaler expects data of the form [n_samples, n_features]
+            flat_data = self.data.reshape(-1, self.nb_features)
+            self.data = self.scaler.fit_transform(flat_data)
+            self.rescaled = True
 
     def explode_into_training_pieces(self, nb_past_timesteps):
+        if nb_past_timesteps + 1 > self.nb_timesteps:
+            raise ValueError('You are asking for too many past timesteps!')
         X_data, y_data = [], []
         for trajectory in range(self.nb_trajectories):
-            for oldest_timestep in range(self.nb_timesteps-nb_past_timesteps-1):
-                X_placeholder = self.data[trajectory, oldest_timestep:(oldest_timestep+nb_past_timesteps), :]
-                y_placeholder = self.data[trajectory, oldest_timestep+nb_past_timesteps, :]
+            for oldest_timestep in range(self.nb_timesteps - nb_past_timesteps - 1):
+                X_placeholder = self.data[trajectory, oldest_timestep:(oldest_timestep + nb_past_timesteps), :]
+                y_placeholder = self.data[trajectory, oldest_timestep + nb_past_timesteps, :]
                 X_data.append(X_placeholder)
                 y_data.append(y_placeholder)
         self.X_data = np.array(X_data)
--- tests/classes/test_TimeseriesDataset.py
+++ tests/classes/test_TimeseriesDataset.py
@@ -17,36 +17,58 @@ class Test_TimeSeriesDataset_with_Valid_Input(unittest.TestCase):
     def test_init_with_valid_address(self):
         timeseries_dataset = TimeSeriesDataset(self.dataset_address)
 
-    def test_remove_timestamps(self):
+    def test_remove_timestamps_once(self):
         timeseries_dataset = TimeSeriesDataset(self.dataset_address, with_timestamps=True)
         nb_features = timeseries_dataset.nb_features
         timeseries_dataset.remove_timestamps()
         self.assertEqual(nb_features-1, timeseries_dataset.nb_features)
 
     def test_remove_timestamps_iterated(self):
-        timeseries_dataset = TimeSeriesDataset(self.dataset_address, with_timestamps=True)
+        timeseries_dataset = TimeSeriesDataset(self.dataset_address, with_timestamps=False)
         nb_features = timeseries_dataset.nb_features
         for j in range(5):
             timeseries_dataset.remove_timestamps()
-        self.assertEqual(nb_features-1, timeseries_dataset.nb_features)
+        self.assertEqual(nb_features, timeseries_dataset.nb_features)
+
+    def test_if_scaler_is_fitted(self):
+        timeseries_dataset = TimeSeriesDataset(self.dataset_address)
+        timeseries_dataset.rescale()
+        scaler = timeseries_dataset.scaler
+        self.assertTrue(hasattr(scaler, 'mean_'))
+        self.assertTrue(hasattr(scaler, 'scale_'))
 
     def tearDown(self):
         os.remove(self.dataset_address)
 
 class Test_TimeSeriesDataset_with_Invalid_Input(unittest.TestCase):
 
+    dataset_address = 'data.npy'
+
     def test_init_with_invalid_address(self):
-        invalid_dataset_address = 'data.npy'
+        invalid_dataset_address = self.dataset_address
         with self.assertRaises(FileNotFoundError):
             TimeSeriesDataset(invalid_dataset_address)
 
     def test_init_with_invalid_shape(self):
         data = np.random.rand(2, 5)
-        dataset_address = 'data.npy'
-        with open(dataset_address, 'wb') as data_file:
+        with open(self.dataset_address, 'wb') as data_file:
             np.save(data_file, data)
         with self.assertRaises(ShapeError):
-            TimeSeriesDataset(dataset_address)
+            TimeSeriesDataset(self.dataset_address)
+
+    def test_explode_into_training_pieces_with_invalid_nb_of_past_timesteps(self):
+        data = np.random.rand(2, 3, 5)
+        with open(self.dataset_address, 'wb') as data_file:
+            np.save(data_file, data)
+        timeseries_dataset = TimeSeriesDataset(self.dataset_address)
+        with self.assertRaises(ValueError):
+            timeseries_dataset.explode_into_training_pieces(4)
+
+    def tearDown(self):
+        try:
+            os.remove(self.dataset_address)
+        except:
+            pass
 
 if __name__ == '__main__':
     unittest.main()

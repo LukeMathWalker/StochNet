1d43fa9d5871dd6dd9eff03fcc09b8b12850f2c3
LukeMathWalker <LukeMathWalker@users.noreply.github.com>
2017-03-10 19:04:43 +0100

    “Fixes”

--- stochnet/applicative/SIR_model_analysis.py
+++ stochnet/applicative/SIR_model_analysis.py
@@ -12,12 +12,14 @@ current = os.getcwd()
 working_path = os.path.dirname(current)
 basename = os.path.abspath(working_path)
 
+nb_past_timesteps = 3
+
 dataset_address = os.path.join(basename, 'dataset/dataset_00.npy')
 dataset = TimeSeriesDataset(dataset_address)
-dataset.format_dataset_for_ML(nb_past_timesteps=3)
+dataset.format_dataset_for_ML(nb_past_timesteps=nb_past_timesteps)
 dataset.train_test_split(percentage_of_test_data=0.25)
 
-input_tensor = Input(shape=(dataset.nb_timesteps, dataset.nb_features))
+input_tensor = Input(shape=(nb_past_timesteps, dataset.nb_features))
 hidden1 = LSTM(150)(input_tensor)
 NN_body = Dense(75)(hidden1)
 
@@ -28,3 +30,4 @@ for j in range(number_of_components):
 TopModel_obj = MixtureOutputLayer(components)
 
 NN = StochNeuralNetwork(input_tensor, NN_body, TopModel_obj)
+NN.fit(dataset.X_train, dataset.y_train)

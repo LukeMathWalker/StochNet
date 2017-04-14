386841df7abe88d86d46393291fda510ba75fa7b
LukeMathWalker <LukeMathWalker@users.noreply.github.com>
2017-04-14 14:53:58 +0200

    Fixes. Put best model in SIR_model_w_LogNormals.py

--- stochnet/applicative/SIR_model_w_LogNormals.py
+++ stochnet/applicative/SIR_model_w_LogNormals.py
@@ -3,7 +3,7 @@ from tensorflow.python import debug as tf_debug
 from stochnet.classes.TimeSeriesDataset import TimeSeriesDataset
 from stochnet.classes.NeuralNetworks import StochNeuralNetwork
 from stochnet.classes.TopLayers import MultivariateNormalCholeskyOutputLayer, MultivariateLogNormalOutputLayer, MixtureOutputLayer
-from keras.layers import Input, LSTM, Dense
+from keras.layers import Input, LSTM, Dense, Dropout
 from keras.callbacks import EarlyStopping
 from keras.constraints import maxnorm
 from keras import backend as K
@@ -25,22 +25,22 @@ data_labels = {'Timestamps': 0, 'Susceptible': 1, 'Infected': 2, 'Removed': 3}
 
 dataset = TimeSeriesDataset(dataset_address, labels=data_labels)
 
-nb_past_timesteps = 2
+nb_past_timesteps = 5
 dataset.format_dataset_for_ML(nb_past_timesteps=nb_past_timesteps, must_be_rescaled=True, positivity='needed', percentage_of_test_data=0.25)
 
 input_tensor = Input(shape=(nb_past_timesteps, dataset.nb_features))
-hidden1 = LSTM(150, kernel_constraint=maxnorm(5), recurrent_constraint=maxnorm(5))(input_tensor)
-NN_body = Dense(75, kernel_constraint=maxnorm(5))(hidden1)
+hidden1 = LSTM(64, kernel_constraint=maxnorm(2.22175262), recurrent_constraint=maxnorm(2.47433967))(input_tensor)
+dropout1 = Dropout(0.46178651)(hidden1)
+dense1 = Dense(128, kernel_constraint=maxnorm(2.30359363))(dropout1)
+dropout2 = Dropout(0.62220663)(dense1)
+NN_body = Dense(512, kernel_constraint=maxnorm(1.87423252))(dropout2)
 
 number_of_components = 2
 components = []
-# for j in range(number_of_components):
-#     components.append(MultivariateLogNormalOutputLayer(dataset.nb_features))
-components.append(MultivariateLogNormalOutputLayer(dataset.nb_features))
-components.append(MultivariateNormalCholeskyOutputLayer(dataset.nb_features))
-TopModel_obj = MixtureOutputLayer(components)
+for j in range(number_of_components):
+    components.append(MultivariateLogNormalOutputLayer(dataset.nb_features))
 
-#TopModel_obj = MultivariateLogNormalOutputLayer(dataset.nb_features)
+TopModel_obj = MixtureOutputLayer(components)
 
 NN = StochNeuralNetwork(input_tensor, NN_body, TopModel_obj)
 callbacks = [EarlyStopping(monitor='val_loss', patience=2, verbose=1, mode='min')]

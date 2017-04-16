f50c8bf2875ec21c6a7bfb46d28467228d5a97aa
LukeMathWalker <LukeMathWalker@users.noreply.github.com>
2017-04-16 09:40:12 +0200

    reorganization

--- stochnet/applicative/SIR_model_analysis.py
+++ stochnet/applicative/SIR_model_analysis.py
@@ -1,9 +1,15 @@
 import os
 from stochnet.classes.TimeSeriesDataset import TimeSeriesDataset
 from stochnet.classes.NeuralNetworks import StochNeuralNetwork
-from stochnet.classes.TopLayers import MultivariateNormalCholeskyOutputLayer, MixtureOutputLayer
-from keras.layers import Input, LSTM, Dense
+from stochnet.classes.TopLayers import MultivariateNormalCholeskyOutputLayer, MultivariateLogNormalOutputLayer, MixtureOutputLayer
+from stochnet.utils.iterator import NumpyArrayIterator
+from keras.layers import Input, LSTM, Dense, Dropout
 from keras.callbacks import EarlyStopping
+from keras.constraints import maxnorm
+
+# sess = tf.Session()
+# sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+# K.set_session(sess)
 
 # We need to get to the proper directory first
 # We are in ./stochnet/applicative
@@ -12,30 +18,49 @@ from keras.callbacks import EarlyStopping
 current = os.getcwd()
 working_path = os.path.dirname(current)
 basename = os.path.abspath(working_path)
-dataset_address = os.path.join(basename, 'dataset/SIR_dataset.npy')
+dataset_address = os.path.join(basename, 'dataset/SIR_dataset_medium.npy')
+test_dataset_address = os.path.join(basename, 'dataset/SIR_dataset_upgraded.npy')
+
 data_labels = {'Timestamps': 0, 'Susceptible': 1, 'Infected': 2, 'Removed': 3}
 
 dataset = TimeSeriesDataset(dataset_address, labels=data_labels)
+test_dataset = TimeSeriesDataset(test_dataset_address, labels=data_labels)
 
 nb_past_timesteps = 5
-dataset.format_dataset_for_ML(nb_past_timesteps=nb_past_timesteps, percentage_of_test_data=0.25)
+dataset.format_dataset_for_ML(nb_past_timesteps=nb_past_timesteps, must_be_rescaled=True, percentage_of_test_data=0)
+test_dataset.format_dataset_for_ML(nb_past_timesteps=nb_past_timesteps, must_be_rescaled=True, percentage_of_test_data=0)
+
+training_generator = NumpyArrayIterator(dataset.X_train, dataset.y_train, batch_size=512, shuffle=False)
+validation_generator = NumpyArrayIterator(test_dataset.X_train, test_dataset.y_train, batch_size=512, shuffle=False)
 
 input_tensor = Input(shape=(nb_past_timesteps, dataset.nb_features))
-hidden1 = LSTM(150, return_sequences=True)(input_tensor)
-hidden2 = LSTM(150)(hidden1)
-NN_body = Dense(75)(hidden2)
+hidden1 = LSTM(128, kernel_constraint=maxnorm(1.78998725), recurrent_constraint=maxnorm(2.95163704))(input_tensor)
+dropout1 = Dropout(0.46178651)(hidden1)
+dense1 = Dense(512, kernel_constraint=maxnorm(1.57732507))(dropout1)
+dropout2 = Dropout(0.62220663)(dense1)
+NN_body = Dense(128, kernel_constraint=maxnorm(1.67525276))(dropout2)
 
 number_of_components = 2
 components = []
 for j in range(number_of_components):
     components.append(MultivariateNormalCholeskyOutputLayer(dataset.nb_features))
+
 TopModel_obj = MixtureOutputLayer(components)
 
 NN = StochNeuralNetwork(input_tensor, NN_body, TopModel_obj)
-callbacks = [EarlyStopping(monitor='val_loss', patience=4, verbose=1, mode='min')]
-NN.fit(dataset.X_train, dataset.y_train, epochs=50, validation_split=0.2, callbacks=callbacks)
+callbacks = [EarlyStopping(monitor='val_loss', patience=7, verbose=1, mode='min')]
+# result = NN.fit(dataset.X_train, dataset.y_train, batch_size=512, epochs=20, callbacks=callbacks, validation_data=(test_dataset.X_train, test_dataset.y_train))
+result = NN.fit_generator(training_generator=training_generator,
+                          samples_per_epoch=10**6, epochs=60, verbose=1,
+                          callbacks=callbacks, validation_generator=validation_generator,
+                          nb_val_samples=10**4)
+lowest_val_loss = min(result.history['val_loss'])
+print(lowest_val_loss)
 
-test_set_prediction = NN.predict(dataset.X_test)
-NN.visualize_performance_by_sampling(dataset.X_test, dataset.y_test, test_set_prediction,
-                                     max_display=2, fitted_scaler=dataset.scaler,
+test_set_prediction = NN.predict(test_dataset.X_train)
+NN.visualize_performance_by_sampling(test_dataset.X_train, test_dataset.y_train, test_set_prediction,
+                                     max_display=4, fitted_scaler=dataset.scaler,
                                      feature_labels=dataset.labels)
+
+filepath_for_saving = os.path.join(basename, 'models/SIR_' + str(lowest_val_loss) + '.h5')
+NN.save(filepath_for_saving)

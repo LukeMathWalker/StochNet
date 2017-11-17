Fix in the histogram computation

stochnet/utils/histograms.py
@@ -13,10 +13,10 @@ def histogram_distance(h_X, h_Y, interval_length):
 #     return np.sort(valid_samples)
 
 
-def get_histogram(samples, histogram_bounds, n_bins, n_dims):
+def get_histogram(samples, histogram_bounds, n_bins, n_dims, bin_length):
     if n_dims > 1:
         hist, edges = np.histogramdd(samples, bins=n_bins, range=histogram_bounds)
     else:
         hist, edges = np.histogram(samples, bins=n_bins, range=histogram_bounds)
     nb_samples = samples.shape[0]
-    return hist / nb_samples
+    return hist / (nb_samples * bin_length)
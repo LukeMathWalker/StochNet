4bc57b129e8e7db90fcf928ccbf4f42be258d34a
LukeMathWalker <lucapalmieri1993@hotmail.com>
2017-11-22 06:05:19 +0000

    Modified the path to retrieve python executables

--- stochnet/utils/envs.py
+++ stochnet/utils/envs.py
@@ -1,6 +1,8 @@
+import os
+
 def get_python_2_env():
-    return '/home/lucap/anaconda3/envs/py2'
+    return '/home/' + os.environ['USER'] + '/anaconda3/envs/py2'
 
 
 def get_python_3_env():
-    return '/home/lucap/anaconda3'
+    return '/home/' + os.environ['USER'] + '/anaconda3'
